0c8260cbf9a6e35d9bc26725bddcc18e03452229
Sebastian Valverde <juansebas_9711@hotmail.com>
2025-05-29 17:23:48 -0500

    Se agrega campo city id en el modelo

--- backend/models/site.js
+++ backend/models/site.js
@@ -6,9 +6,17 @@ const Site = sequelize.define('Site', {
   name: DataTypes.STRING,
   type: DataTypes.STRING,
   description: DataTypes.TEXT,
+  city_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: City,
+      key: 'id'
+    }
+  }
 });
 
-Site.belongsTo(City);
-City.hasMany(Site);
+Site.belongsTo(City, { foreignKey: 'city_id' });
+City.hasMany(Site, { foreignKey: 'city_id' });
 
 module.exports = Site;
